db2c3043a4df708c922953f760f78a42bbebad82
gregmaggio <gregorymaggio@gmail.com>
2020-06-03 10:22:48 -0400

    Added CORS for Angular Development

--- src/main/java/ca/datamagic/root/servlet/WhoAmIServlet.java
+++ src/main/java/ca/datamagic/root/servlet/WhoAmIServlet.java
@@ -30,8 +30,31 @@ public class WhoAmIServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static Logger logger = LogManager.getLogger(WhoAmIServlet.class);
 	
+	private static void addCorsHeaders(String origin, HttpServletResponse response) {
+		if ((origin != null) && (origin.length() > 0)) {
+			if (origin.compareToIgnoreCase("http://localhost:4200") == 0) {
+				response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+				response.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+				response.setHeader("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization, X-Request-With, location");
+				response.setHeader("Access-Control-Allow-Credentials", "true");
+			}
+		}
+	}
+	
+	@Override
+	protected void doOptions(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String origin = request.getHeader("Origin");
+		logger.debug("origin: " + origin);
+		addCorsHeaders(origin, response);
+		response.setHeader("Content-Length", "0");
+		response.setHeader("Content-Type", "text/plain");
+		response.setStatus(HttpServletResponse.SC_OK);
+	}
+	
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String origin = request.getHeader("Origin");
+		logger.debug("origin: " + origin);
 		OAuthDAO dao = new OAuthDAO();
 		String sessionID = null;
 		Cookie[] cookies = request.getCookies();
@@ -42,6 +65,7 @@ public class WhoAmIServlet extends HttpServlet {
 			}
 		}
 		if (sessionID == null) {
+			addCorsHeaders(origin, response);
 			response.sendError(401);
 			return;
 		}
@@ -50,8 +74,10 @@ public class WhoAmIServlet extends HttpServlet {
 			LoginSessionDTO loginSession = loginSessionDAO.read(sessionID);
 			String now = UTCUtils.getTimeStamp();
 			if (now.compareToIgnoreCase(loginSession.getExpireDate()) > 0) {
+				addCorsHeaders(origin, response);
 				response.sendError(401);
 			} else {
+				addCorsHeaders(origin, response);
 				response.setContentType("application/json");
 				response.getWriter().print((new Gson()).toJson(loginSession.getUserName()));
 			}
